f9cb58b58354e78dde42ba399ddee970092be35a
シン・ドングン <shindongkeun@shindongunnoMacBook-Air.local>
2023-01-17 17:52:20 +0900

    next

---
+++ pages/index.tsx
@@ -0,0 +1,59 @@
+import type { GetStaticProps } from "next"
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import { getSortedPostsData } from "../lib/post"
+import homeStyles from '../styles/Home.module.css'
+import Link from "next/link"
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Home = ({allPostsData}: {
+  allPostsData: {
+    date: string
+    title: string
+    id : string
+  }[]
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>John Ahn</title>
+      </Head>
+      <section className='={homeStyles.headingMd'>
+        <p>[john Ahn Introduction]</p>
+        <p>
+          (This is a website)
+        </p>
+      </section>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}> 
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({id, title, date}) => 
+          <li className={homeStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+              <br />
+              <small className={homeStyles.lightText}>
+                {date}
+              </small>
+          </li>
+          )}
+        </ul>
+      </section>
+
+    </div>
+  )
+}
+
+export default Home
+
+export const getStaticProps : GetStaticProps =async() => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
